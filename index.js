85d39e10c75171479dc2be04155393f3c277b128
josueperez723@gmail.com <Josue Perez>
2025-02-16 09:47:23 -0400

    final

--- index.js
+++ index.js
@@ -1,33 +1,47 @@
-const content = document.getElementById('elemento');
-const respuesta = document.querySelector('#respuesta');
-const boton = document.querySelector('#boton');
-const botonTexto = document.querySelector('#botonTexto');
-const imgPlus = document.createElement('img')
-const imgMinus = document.createElement('img')
+document.addEventListener('DOMContentLoaded', () => {
+    const faqElements = document.querySelectorAll('.FAQ-element');
+    const boton = document.querySelectorAll('.FAQ-button');
 
+    boton.forEach((element) => {
+        const imgPlus = document.createElement('img')
+        imgPlus.setAttribute('src', './assets/images/icon-plus.svg');
+        imgPlus.setAttribute('class', 'plus');
+        imgPlus.setAttribute('alt', 'plus icon');
+        element.append(imgPlus);
+        const imgMinus = document.createElement('img')
+        imgMinus.setAttribute('src', './assets/images/icon-minus.svg');
+        imgMinus.setAttribute('class', 'minus minus-inactive');
+        imgMinus.setAttribute('alt', 'minus icon');
+        element.append(imgMinus);
+    });
+    faqElements.forEach((element) => {
+        const faqQuestion = element.querySelector('p');
+        const faqAnswer = element.querySelector('div');
+        const faqButton = element.querySelector('button');
+        element.addEventListener('click', function () {
+            toggleClass(faqAnswer)
+            const imgPlus = faqButton.querySelector('img.plus');
+            const imgMinus = faqButton.querySelector('img.minus-inactive');
+            const imgMinusActive = faqButton.querySelector('img.minus');
+            if (imgMinus) {
+                imgMinus.classList.remove('minus-inactive');
+                imgPlus.classList.add('plus-inactive');
+            } else {
+                imgPlus.classList.remove('plus-inactive');
+                imgMinusActive.classList.add('minus-inactive');
+            }
+        });
+        //? si llamo a la funcion directamente se ejecuta inmediatamente, asi que tengo q llamar al toggle como funcion anonima
 
-imgPlus.setAttribute('src', './assets/images/icon-plus.svg')
-imgMinus.setAttribute('src', './assets/images/icon-minus.svg')
-
-
-boton.append(imgPlus);
-
-content.addEventListener('click', toggleClass);
-content.addEventListener('mouseover', function() {
-    botonTexto.setAttribute('style', 'color: #8c6991');
+        element.addEventListener('mouseover', function () {
+            faqQuestion.setAttribute('style', 'color: #8c6991');
+        });
+        element.addEventListener('mouseout', function () {
+            faqQuestion.removeAttribute('style');
+        });
+    });
 });
 
-content.addEventListener('mouseout', function() {
-    botonTexto.removeAttribute('style');
-});
-
-function toggleClass() {
+function toggleClass(respuesta) {
     respuesta.classList.toggle('inactive');
-    if (boton.contains(imgPlus)) {
-        boton.removeChild(imgPlus);
-        boton.append(imgMinus);
-    } else {
-        boton.removeChild(imgMinus);
-        boton.append(imgPlus);
-    }
 }
